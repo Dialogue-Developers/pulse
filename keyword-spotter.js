c369bc88b546f2580aa4b8daa5ef143512840f5a
Elmer Dema <elmerdema2022@gmail.com>
2023-05-14 17:39:27 +0200

    response speed improved

--- keyword-spotter.js
+++ keyword-spotter.js
@@ -29,17 +29,27 @@ export async function keywordSpotter(message) {
 		let response = null;
 		let do_next = null;
 
-		// TODO: Optimize this (O(n^3) is not good)
+		const responseMap = {};
 		responses.forEach((response_object) => {
 			response_object.keyword.forEach((keyword) => {
-				words.forEach((word) => {
-					if (word.toLowerCase() == keyword.toLowerCase()) {
-						response = response_object.responses[Math.floor(Math.random() * response_object.responses.length)];
-						do_next = response_object.do_next;
-					}
-				});
+				const keywordLower = keyword.toLowerCase();
+				if (!responseMap[keywordLower]) {
+					responseMap[keywordLower] = {
+						responses: response_object.responses,
+						do_next: response_object.do_next
+					};
+				}
 			});
 		});
+		for (const word of words) {
+			const wordLower = word.toLowerCase();
+			if (responseMap[wordLower]) {
+				const responseObj = responseMap[wordLower];
+				const response = responseObj.responses[Math.floor(Math.random() * responseObj.responses.length)];
+				do_next = responseObj.do_next;
+				break;
+			}
+		}
 
 		if (response == null) {
 			return null;
